1cdbb5b60a6fec2c67356e24aca46b3f8cc2aad0
bikash sarker <bikash.webdeveloper5@gmail.com>
2025-05-22 19:19:47 +0600

    the api intrigation

---
+++ ai.js
@@ -0,0 +1,14 @@
+import { GoogleGenAI } from "@google/genai";
+const G_API_KEY = process.env.GEMINI_API_KEY;
+
+const ai = new GoogleGenAI({ apiKey: G_API_KEY });
+
+async function main() {
+  const response = await ai.models.generateContent({
+    model: "gemini-2.0-flash-001",
+    contents: "Why is the sky blue?",
+  });
+  console.log(response.text);
+}
+
+export default main;
--- index.js
+++ index.js
@@ -1,8 +1,30 @@
 require("dotenv").config();
+const { GoogleGenerativeAI } = require("@google/generative-ai");
 const express = require("express");
 const app = express();
 
 const port = process.env.SERVER_PORT;
+
+const G_API_KEY = process.env.GEMINI_API_KEY;
+const genAI = new GoogleGenerativeAI(G_API_KEY);
+const model = genAI.getGenerativeModel({
+  model: "gemini-1.5-flash",
+  systemInstruction: "He is promptHub Ai. he is new platform.",
+});
+
+app.get("/ai", async (req, res) => {
+  const kotha = "what is AI ";
+  const prompt = req.query?.prompt;
+
+  if (!prompt) {
+    req.send({ message: "please you are write" });
+    return;
+  }
+  const result = await model.generateContent(prompt);
+  const response = await result.response;
+  res.send({ result: response.text() });
+  console.log({ result: response.text() });
+});
 
 app.get("/", async (req, res) => {
   res.send({ message: "we are check the server" });
